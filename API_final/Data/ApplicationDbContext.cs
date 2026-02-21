f999cf52ecb8aa790e240760ae478e95bef75ff4
Gerónimo Massera <gerocabrer37@gmail.com>
2026-02-21 16:24:37 -0300

    DbContext

---
+++ API_final/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using API_final.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace API_final.Data
+{   
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuración defensiva para evitar errores de borrado en cascada
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Products)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
