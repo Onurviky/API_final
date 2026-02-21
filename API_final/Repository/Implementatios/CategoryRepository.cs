982ac9487c9b89ecc2827a1aedc65238880cbdd4
Gerónimo Massera <gerocabrer37@gmail.com>
2026-02-21 17:26:18 -0300

    Category Repository + Interface

---
+++ API_final/Repository/Implementatios/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using API_final.Data;
+using API_final.Entities;
+using API_final.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_final.Repository.Implementatios
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetByRestaurantAsync(int userId)
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .Where(c => c.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Category category)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ API_final/Repository/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using API_final.Entities;
+
+namespace API_final.Repository.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetByRestaurantAsync(int userId);
+        Task<Category?> GetByIdAsync(int id);
+        Task<Category> AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(Category category);
+    }
+}
